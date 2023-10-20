280581582401ea6811d8c9bebedb858abaa2f63d
Sylvain ZEZE <sylvain.zeze@una-ufrsfa.ci>
2023-10-20 11:23:41 +0000

    GUIDE

---
+++ guide.php
@@ -0,0 +1,98 @@
+<h1>
+    GUIDE
+</h1>
+<h2>
+    Paramètres
+</h2>
+<ul>
+    <li>
+        Hôte: host = <strong>
+            una-ufrsfa.ci
+        </strong>
+    </li>
+    <li>
+        Domaine = <strong>
+            l3info-2024.una-ufrsfa.ci
+        </strong>
+    </li>
+    <li>
+        Pseudo = <strong>
+            prenom.nom
+        </strong>
+    </li>
+</ul>
+<h2>
+    Compte FTP
+</h2>
+<ul>
+    <li>
+        Hôte: <strong>
+            una-ufrsfa.ci
+        </strong>
+    </li>
+    <li>
+        User: <strong>
+            pseudo@l3info-2024.una-ufrsfa.ci
+        </strong>
+    </li>
+    <li>
+        Mot de passe: <strong>
+            réçu par message
+        </strong>
+    </li>
+</ul>
+
+<h2>
+    WEB
+</h2>
+
+<ul>
+    <li>
+        http://www.<strong>pseudo</strong>.l3info-2024.una-ufrsfa.ci
+    </li>
+</ul>
+<h2>
+    Travail à faire: Projets
+</h2>
+
+<ol>
+    <li>
+        <h3>
+            Annuaire: Projet CSS
+        </h3>
+        Il s'agit de styliser l'annuaire de la classe en créant un fichier <strong>annuaire.css</strong> à la racine de
+        votre dossier
+    </li>
+
+    <li>
+        <h3>
+            CV
+        </h3>
+        Vous devez créer votre CV à votre goût
+    </li>
+
+
+</ol>
+<div>
+
+    <p style="background-color: yellow; color:black;">
+        <span style="font-weight: bold;">N.B.</span>
+        Toutes les informations vous concernant (CV et annuaire) devront provenir de votre fichier data.json qui doit se
+        trouver à la
+        racine de votre dossier: <br>
+        Vous devez créer un fichier data.json. <br>
+        Critères de la photo: <br>
+    <ul style="background-color: yellow;">
+        <li>
+            Emplacement: à la racine de votre dossier
+        </li>
+        <li>
+            Nom: photo.jpg
+        </li>
+        <li>
+            Dimension: 600px x 600px
+        </li>
+    </ul>
+
+    </p>
+</div>
